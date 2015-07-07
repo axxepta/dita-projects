fa8e463497e18c8ac6458fbf0e8b6c0cb63833db
mgcalo <magda.caloian@pantopix.de>
2015-07-07 19:17:08 +0200

    sample topics, ditamaps and submaps (in progress)

---
+++ trello/out/webhelp-trello-simple/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,9 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "source/t_agile-boards.html";
+fl["1"]= "source/t_brainstorming.html";
+fl["2"]= "source/t_keeping-to-do-lists.html";
+fl["3"]= "source/t_ov_planning-your-projects-with-trello.html";
+fl["4"]= "source/t_voting.html";
+fl["5"]= "source/t_writing-drafts.html";
+var doStem = false
