8fd1c7aa8827edf853a2de79dd5f807b3a3fb0de
Max Gaerber <gaerber@axxepta.de>
2015-10-14 21:06:43 +0200

    add basex pom and init script

--- trello/out/webhelp-trello-simple/oxygen-webhelp/search/htmlFileInfoList.js
+++
@@ -1,7 +0,0 @@
-fil = new Array();
-fil["0"]= "source/t_agile-boards.html@@@Agile boards@@@When working with agile teams, create an Agile Board to suit your workflow...";
-fil["1"]= "source/t_brainstorming.html@@@Brainstorming@@@Trello can be used as an interactive brainstorming board, for storing ideas, grouping and prioritizing them...";
-fil["2"]= "source/t_keeping-to-do-lists.html@@@Keeping to-do lists@@@Keep track of your chores and errands, of reading lists, of travel arrangements, etc. on To Do boards...";
-fil["3"]= "source/t_ov_planning-your-projects-with-trello.html@@@Planning your projects with Trello@@@Trello is a virtual, interactive board you can use for personal and business projects, alone and in teams, in private and shared mode...";
-fil["4"]= "source/t_voting.html@@@Voting@@@Allow members to vote on cards and prioritize tasks and ideas...";
-fil["5"]= "source/t_writing-drafts.html@@@Writing drafts@@@When researching and writing content, create a planning board...";
